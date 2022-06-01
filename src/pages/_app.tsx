2ad14e7343b8df411c2c33b9a0d7d6563e0aad1d
Samuel Hepworth <samhep0803@gmail.com>
2022-06-01 21:29:32 +0400

    fix(ui): hotfix for unauthed user login redirect (#7)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import { useUser } from "@/lib/user";
 import theme from "@/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 import { NextPage } from "next";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { ReactElement, ReactNode } from "react";
 import { Layout } from "../layouts/Layout";
 
@@ -14,6 +16,13 @@ type AppPropsWithLayout = AppProps & {
 };
 
 const defaultLayout = (page: ReactElement) => {
+	const router = useRouter();
+	const user = useUser();
+
+	if (user === null) {
+		router.push("/login");
+		console.log(user || "no user");
+	}
 	return (
 		<ChakraProvider resetCSS theme={theme}>
 			<style>
