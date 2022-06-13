47ce614c5a65a88f19f6ff93041de9247a901bce
Samuel Hepworth <samhep0803@gmail.com>
2022-06-13 21:31:15 +0400

    in-prog(ui): remove index header because mubarak is a chutiya bhenchod bhosdike tu meri baap ka chaddi he

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-import { RatingBadge } from "@/components/RatingBadge";
 import { useUser } from "@/lib/user";
-import { Flex, Heading } from "@chakra-ui/react";
 import { NextPage } from "next";
 
 const Index: NextPage = () => {
@@ -10,35 +8,7 @@ const Index: NextPage = () => {
 		return null;
 	}
 
-	return (
-		<Flex w={"100%"} flexDir={"column"} h={"100%"}>
-			<Flex
-				borderRadius={4}
-				justifyContent={"center"}
-				w={"100%"}
-				h={{ base: "10%" }}
-				backgroundImage={"/header.png"}
-				backgroundSize={"cover"}
-				backgroundPosition={"50% 40%"}
-			>
-				<Flex
-					w={"100%"}
-					h={"100%"}
-					justifyContent={"center"}
-					alignItems={"center"}
-					background={"rgba(0,0,0,0.65)"}
-				>
-					<Heading fontSize={{ base: "2xl", sm: "3xl" }}>
-						<Heading as="span" textColor={"gray.200"} fontSize={"3xl"}>
-							Welcome,{" "}
-						</Heading>
-						{user.personal.name_full} -{" "}
-						<RatingBadge rating={user.vatsim.rating.id} fontSize={"xl"} />
-					</Heading>
-				</Flex>
-			</Flex>
-		</Flex>
-	);
+	return <div> fuck u</div>;
 };
 
 export default Index;
