801cb66796c8a079c406ec5321dfe6af7fd63bdf
Samuel Hepworth <samhep0803@gmail.com>
2022-06-20 22:21:17 +0400

    fix(api): better way of filtering MENA pilots and controllers

--- src/utils/getMENA.ts
+++ src/utils/getMENA.ts
@@ -27,29 +27,39 @@ export const getMENAControllers = (controllers: Controller[]): Controller[] => {
 };
 
 const checkAirport = (airport: string): boolean => {
-	return (
-		airport.startsWith("OM") ||
-		airport.startsWith("OT") ||
-		airport.startsWith("DOH") ||
-		airport.startsWith("OB") ||
-		airport.startsWith("OI") ||
-		airport.startsWith("OY") ||
-		airport.startsWith("OO") ||
-		airport.startsWith("OE") ||
-		airport.startsWith("OJ") ||
-		airport.startsWith("OL") ||
-		airport.startsWith("OR") ||
-		airport.startsWith("OK") ||
-		airport.startsWith("HE") ||
-		airport.startsWith("HL") ||
-		airport.startsWith("HS") ||
-		airport.startsWith("HA") ||
-		airport.startsWith("HH") ||
-		airport.startsWith("HC") ||
-		airport.startsWith("DA") ||
-		airport.startsWith("DT") ||
-		airport.startsWith("DR") ||
-		airport.startsWith("GM") ||
-		airport.startsWith("FT")
-	);
+	const regexExps = Object.values(callsigns);
+	return regexExps.some((regexExp) => {
+		if (airport.match(regexExp)) {
+			return true;
+		}
+	});
+};
+
+const callsigns = {
+	HECC: /(^HE[A-Z]{2}((.+)?)$)/,
+	OJAC: /(^OJ[A-Z]{2}(.+)?$)|(^AMM_APP$)/,
+	OMAE: /(^OM[A-Z]{2}(.+)?$)/,
+	OBBB: /(^OB[A-Z]{2}(.+)?$)|(^DOH_(?:._|)APP$)|(^OT[A-Z]{2}(.+)?$)/,
+	OKAC: /(^OK[A-Z]{2}(.+)?$)/,
+	ORBB: /(^OR[A-Z]{2}(.+)?$)/,
+	OIIX: /(^OI[A-Z]{2}(.+)?$)|(^TEH_.+$)/,
+	OSTT: /(^OS[A-Z]{2}(.+)?$)/,
+	OOMM: /(^OO[A-Z]{2}(.+)?$)/,
+	OYSC: /(^OY[A-Z]{2}(.+)?$)/,
+	OEJN: /(^OE[A-Z]{2}(.+)?$)/,
+	OLBB: /(^OL[A-Z]{2}(.+)?$)/,
+	GULF: /^GULF_(E_|W_|)FSS$/,
+	AFR: /^AFR(N|E|)_FSS$/,
+	MENA: /^MENA_(E_|W_|S_|C_)FSS$/,
+
+	HLLL: /(^HL[A-Z]{2}(.+)?$)/,
+	HSSS: /(^HS[A-Z]{2}(.+)?$)/,
+	HAAA: /(^HA[A-Z]{2}(.+)?$)/,
+	HHAA: /(^HH[A-Z]{2}(.+)?$)/,
+	HCSM: /(^HC[A-Z]{2}(.+)?$)/,
+	DTTC: /(^DT[A-Z]{2}(.+)?$)/,
+	DAAA: /(^DA[A-Z]{2}(.+)?$)/,
+	FTTT: /(^FT[A-Z]{2}(.+)?$)/,
+	DRRR: /(^DR[A-Z]{2}(.+)?$)/,
+	GMMM: /(^GM[A-Z]{2}(.+)?$)/,
 };
